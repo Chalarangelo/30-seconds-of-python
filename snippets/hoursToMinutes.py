012aadbcf4a1317dbc20927e57df6be90a91678a
Jake Gerber <jake.j.gerber@gmail.com>
2020-10-10 16:25:43 -0700

    Created minutesToHours and hoursToMinutes snippets

---
+++ snippets/hoursToMinutes.py
@@ -0,0 +1,18 @@
+---
+title: hoursToMinutes
+tags: utility,beginner
+---
+
+This converts hours into minutes.
+
+-Takes the hours and multiplies by 60.
+-Returns the result.
+
+```py
+def hoursToMinutes(hours):
+  return hours*60
+```
+
+```py
+hoursToMinutes(1) # 60
+```
---
+++ snippets/minutesToHours.py
@@ -0,0 +1,18 @@
+---
+title: minutesToHours
+tags: utility,beginner
+---
+
+This converts minutes into hours.
+
+-Takes the minutes and divides by 60.
+-Returns the result.
+
+```py
+def minutesToHours(minutes):
+  return minutes/60
+```
+
+```py
+minutesToHours(60) # 1
+```
